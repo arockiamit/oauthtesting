783b1ff21a27a60459119348d771564753f40cd6
santhosh <santhosh.r@kaaviansys.com>
2022-11-09 12:10:03 +0530

    otp verification

--- client/src/register/register.jsx
+++ client/src/register/register.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import React from "react";
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+import { signInWithPhoneNumber } from "firebase/auth";
+import firebase from './firebase'
 
-export default function Register() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <input type="text" name="name" id="Name" />
-      <input type="number" name="phoneNum" />
-    </div>
-  );
+class App extends React.Component{
+  handleChange = (e) =>{
+    const {name,value}=e.target
+    this.setState({
+        [name]:value
+      })
+  }
+
+  configureCaptcha = () =>{
+    const auth = getAuth();
+window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+  'size': 'invisible',
+  'callback': (response) => {
+    // reCAPTCHA solved, allow signInWithPhoneNumber.
+    this.onSignInSubmit();
+    console.log("captcha verified")
+  },
+  defaultCountry :"IN"
+}, auth);
+  }
+  onSignInSubmit = (e) =>{
+    e.preventDefault()
+    this.configureCaptcha()
+    const phoneNumber ="+91" + this.state.mobile
+    console.log(phoneNumber  )
+    const name=this.state.name
+    console.log(name)
+  
+        
+    const appVerifier = window.recaptchaVerifier;
+    const auth = getAuth();
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+          // SMS sent. Prompt user to type the code from the message, then sign the
+          // user in with confirmationResult.confirm(code).
+          window.confirmationResult = confirmationResult;
+          console.log('otp send');
+          fetch('http://localhost:3001/number', 
+          { method: 'post', body: JSON.stringify({ phoneNumber }), headers: { 'content-type': 'application/json' } })
+          fetch('http://localhost:3001/name', 
+          { method: 'post', body: JSON.stringify({ name }), headers: { 'content-type': 'application/json' } })
+          // ...
+        }).catch((error) => {
+          // Error; SMS not sent
+          // ...
+          console.log('otp not send');
+        });
+  }
+  onSubmitOtp = (e) =>{
+    e.preventDefault()
+    const code = this.state.otp
+    console.log(code);
+window.confirmationResult.confirm(code).then((result) => {
+  // User signed in successfully.
+  const user = result.user;
+  console.log(JSON.stringify(user))
+  alert("user verified")
+  // ...
+}).catch((error) => {
+  // User couldn't sign in (bad verification code?)
+  // ...
+});
+  }
+  render(){
+    return(
+      <div>
+      <h2>PHONE NUMBER</h2>
+      <form onSubmit={this.onSignInSubmit}>
+        <div id="sign-in-button"></div>
+        <input type="number" name="mobile" placeholder="mobile number" required onChange={this.handleChange}  />
+        <input type="text" name="name" placeholder="name" required onChange={this.handleChange}  />
+        <button type="submit">submit</button>
+      </form>
+      <h2>OTP NUMBER</h2>
+      <form onSubmit={this.onSubmitOtp}>
+        <input type="number" name="otp" placeholder="otp number" required onChange={this.handleChange}/>
+        <button type="submit">submit</button>
+      </form>
+      </div>
+    )
+  }
 }
+export default App
--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
 const app = express();
-
+app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
+app.post('/number',(req,res)=>{
+  const {phoneNumber}=req.body;
+  console.log(phoneNumber);
+})
+app.post('/name',(req,res)=>{
+  const {name}=req.body;
+  console.log(name);
+})
+
+
 
 app.listen(3001, () => {
   console.log('server Running');
