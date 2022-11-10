7f1cc28e97999ae43b9e1e06551b355ec6f44c9a
Poomathi <poomathi.k@kaaviansys.com>
2022-11-10 18:03:43 +0530

    Fixing eslinting errors..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {  BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Menu from './Components/Menu';
 
 import Register from './register/register';
