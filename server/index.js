b0798ef25bab20c80683747b598025bc9b9c2549
meena <meenakshi.m@kaaviansys.com>
2022-11-16 12:45:15 +0530

    deletecontactnum

--- server/index.js
+++ server/index.js
@@ -213,6 +213,7 @@ if (NODE_ENV === 'DIT') {
   });
 }
 
-app.listen(3001, () => {
+const server = app.listen(3001, () => {
   console.log('server Running');
 });
+module.exports = { server };
--- server/testFunctions/deleteContactNumber.js
+++ server/testFunctions/deleteContactNumber.js
@@ -1,14 +1,22 @@
 /* eslint-disable no-return-await */
 const { UserDetails } = require('../schema');
 
-async function deleteContactNumber1(token) {
-  await UserDetails.updateOne({ userEmail: token }, { $unset: { contactNumber1: '' } });
-  return await UserDetails.find({}).then((data) => data);
-}
+// async function deleteContactNumber1(token) {
+//   await UserDetails.updateOne({ userEmail: token }, { $unset: { contactNumber1: '' } });
+//   return await UserDetails.find({}).then((data) => data);
+// }
+const deleteContactNumber1 = (token) => new Promise((resolve) => {
+  UserDetails.updateOne({ userEmail: token }, { $unset: { contactNumber1: '' } })
+    .then(() => resolve({ status: 'success' }))
+    .catch((error) => {
+      // console.log(error);
+      resolve({ status: 'error', error });
+    });
+});
+// module.exports = { userRegister };
 
 async function deleteContactNumber2(token) {
-  await UserDetails.updateOne({ userEmail: token }, { $unset: { contactNumber2: '' } });
-  return await UserDetails.find({}).then((data) => data);
+  return await UserDetails.updateOne({ userEmail: token }, { $unset: { contactNumber2: '' } });
 }
 
 async function deleteContactNumber3(token) {
