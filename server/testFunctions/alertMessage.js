6685a49eb3d83e4ee75c8ef2dbf985c206307d56
meena <meenakshi.m@kaaviansys.com>
2022-11-21 09:32:49 +0530

    alertmsgcrted

--- server/testFunctions/alertMessage.js
+++ server/testFunctions/alertMessage.js
@@ -2,20 +2,42 @@
 /* eslint-disable no-console */
 const axios = require('axios');
 
-const alertMessage = (contactNumber, userName, location) => new Promise(() => {
+const alertaxios = async (...args) => new Promise((resolve, reject) => {
+  axios.post(...args)
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+// eslint-disable-next-line consistent-return
+const alertMessage = async (contactNumber, userName, location) => {
   if (contactNumber !== undefined) {
     const data = `{"messaging_product": "whatsapp", "to":${contactNumber}, "type": "template", "template": { "name": "alert_safe_wizards", "language": { "code": "en_US" },"components":[{"type":"body","parameters":[{"type":"text","text":"${userName}"},{"type":"text","text":"${location}"}]}] }}`;
-    const config = {
-      method: 'POST',
-      url: 'https://graph.facebook.com/v15.0/106768935582427/messages',
+
+    const header = {
       headers: {
         'Content-Type': 'application/json',
         Authorization: 'Bearer EAAP6obW3ZB1oBAA1trZChcOXxyNE4c6tdKY99vnDJGzKrooM45TjFDJRjELDmiFPoV2UIa6yPJmsBYM2NxjwJzFWBiaR6X6AiCqsZBQDiahScq8i7SQxYhcgWMZBdaJagdzZB29xEPZC2534b8Bc0eNk40HuSJ3wtsl9LVjRCVtPw9mEWftVWT',
       },
-      data,
     };
-    axios(config);
+    try {
+      // eslint-disable-next-line no-use-before-define
+      const msg = await obj.alertaxios('https://graph.facebook.com/v15.0/106768935582427/messages', data, header);
+      return msg;
+    } catch (err) {
+      throw new Error(err);
+    }
+    // axios.post(config)
+    //   .then(() => resolve({ status: 'success' }))
+    //   .catch((error) => {
+    //     resolve({ status: 'error', error });
+    //   });
+  } else {
+    return ({ status: 'failed' });
   }
-});
-
-module.exports = { alertMessage };
+};
+const obj = { alertMessage, alertaxios };
+module.exports = obj;
