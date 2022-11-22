985e3bab5cf5e8a6ae524e9b9b48a1e33b6b9ada
Poomathi <poomathi.k@kaaviansys.com>
2022-11-22 17:59:50 +0530

    Fixing error in delete Function module..

--- server/testFunctions/deleteContactNumber.js
+++ server/testFunctions/deleteContactNumber.js
@@ -1,35 +1,46 @@
+/* eslint-disable no-else-return */
+/* eslint-disable max-len */
 /* eslint-disable no-return-await */
 const { UserDetails } = require('../schema');
 
 async function deleteContactNumber1(email) {
   if (email) {
     await UserDetails.updateOne({ userEmail: email }, { $unset: { contactNumber1: '' } });
-    // eslint-disable-next-line max-len
     const userData = await UserDetails.findOne({ userEmail: email }, { userEmail: 0, _id: 0, __v: 0 });
     return userData;
+  } else {
+    return ({ status: 'error' });
   }
-  return ({ status: 'error' });
 }
 async function deleteContactNumber2(email) {
   if (email) {
     await UserDetails.updateOne({ userEmail: email }, { $unset: { contactNumber2: '' } });
-    const userData = UserDetails.findOne({ userEmail: email }, { userEmail: 0, _id: 0, __v: 0 });
+    const userData = await UserDetails.findOne({ userEmail: email }, { userEmail: 0, _id: 0, __v: 0 });
     return userData;
-  // eslint-disable-next-line no-else-return
   } else {
     return ({ status: 'error' });
-  // eslint-disable-next-line padded-blocks
   }
-
 }
 async function deleteContactNumber3(email) {
   if (email) {
     await UserDetails.updateOne({ userEmail: email }, { $unset: { contactNumber3: '' } });
-    const userData = UserDetails.findOne({ userEmail: email }, { userEmail: 0, _id: 0, __v: 0 });
+    const userData = await UserDetails.findOne({ userEmail: email }, { userEmail: 0, _id: 0, __v: 0 });
     return userData;
+  } else {
+    return ({ status: 'error' });
   }
-
-  return ({ status: 'error' });
 }
 
-module.exports = { deleteContactNumber1, deleteContactNumber2, deleteContactNumber3 };
+async function deleteCallNumber(email) {
+  if (email) {
+    await UserDetails.updateOne({ userEmail: email }, { $unset: { callingNumber: '' } });
+    const userData = await UserDetails.findOne({ userEmail: email }, { userEmail: 0, _id: 0, __v: 0 });
+    return userData;
+  } else {
+    return ({ status: 'error' });
+  }
+}
+
+module.exports = {
+  deleteContactNumber1, deleteContactNumber2, deleteContactNumber3, deleteCallNumber,
+};
