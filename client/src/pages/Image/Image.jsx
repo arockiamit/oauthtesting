253368e0b2ab2b43a0fe6214c1c39ff78eefe139
Poomathi <poomathi.k@kaaviansys.com>
2022-11-23 16:11:17 +0530

    Changing alertMessage Functions..

--- client/src/pages/Image/Image.jsx
+++ client/src/pages/Image/Image.jsx
@@ -1,24 +1,26 @@
+/* eslint-disable max-len */
 /* eslint-disable no-console */
-import React, { useState } from 'react';
-
+import React from 'react';
+import image from './myImg.png';
 import './image.css';
 
 export default function Imagecapture() {
   // const img = localStorage.getItem('image');
-  const token = localStorage.getItem('accesstoken');
-  const [Data, setData] = useState();
+  // const token = localStorage.getItem('accesstoken');
+  // const [Data, setData] = useState();
 
-  const getImage = () => {
-    fetch(`${process.env.REACT_APP_SERVER_PREFIX}/api/getImage`, { method: 'POST', body: JSON.stringify({ token }), headers: { 'content-type': 'application/json' } })
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.image);
-      });
-  };
+  // const getImage = () => {
+  //   fetch(`${process.env.REACT_APP_SERVER_PREFIX}/api/getImage`, { method: 'POST', body: JSON.stringify({ token }), headers: { 'content-type': 'application/json' } })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data.image);
+  //       console.log(Data);
+  //     });
+  // };
 
-  getImage();
+  // getImage();
   // console.log(img);
   return (
-    <img src={Data} alt="img" className="alertcapturedimage" />
+    <img src={image} alt="img" className="alertcapturedimage" />
   );
 }
